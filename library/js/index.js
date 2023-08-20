a98647fc9d7e06e969ca4c15975c08846072df2f
1g000 <gorbatovskiy.igor@gmail.com>
2023-08-20 22:39:00 +0300

    feat: keep doin slider

--- library/js/index.js
+++ library/js/index.js
@@ -1,3 +1,5 @@
+// Бургер-меню
+
 window.addEventListener("resize", closeBurgerOnResize);
 const burgerBtn = document.querySelector(".burger-menu-icon");
 const burgerMenu = document.querySelector(".burger-menu");
@@ -36,32 +38,41 @@ function closeBurgerOnResize() {
   }
 }
 
-console.log(`
-Вёрстка соответствует макету. Ширина экрана 768px +26 Выполнено\n
-блок <header> +2 Выполнено\n
-секция Welcome +2 Выполнено\n
-секция About +4.Выполнено\n
-секция Favorites +2 Выполнено\n
-Сделать кнопку own,Если условие соблюдено: +2 Выполнено\n
-секция CoffeShop +4 Выполнено\n
-секция Contacts +4 Выполнено\n
-секция LibraryCard +4 Выполнено\n
-блок <footer> + 2 Выполнено\n
+//Слайдер
 
-Ни на одном из разрешений до 640px включительно не появляется\n
-горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется:\n
-не обрезается и не удаляется +12 Выполнено\n
+let offset = 0;
+const slider = document.querySelector(".gallery-items");
+const btnRight = document.querySelector(".arrow-right");
+const btnLeft = document.querySelector(".arrow-left");
 
-нет полосы прокрутки при ширине страницы от 1440рх до 640рх +4 Выполнено\n
-элементы не выходят за пределы окна браузера при ширине страницы от 1440рх до 640рх +4 Выполнено\n
-элементы не наезжают друг на друга при ширине страницы от 1440рх до 640рх +4 Выполнено \n
+btnLeft.disabled = true;
+btnRight.disabled = false;
 
-На ширине экрана 768рх реализовано адаптивное меню +12 Выполнено \n
-при нажатии на бургер-иконку плавно появляется адаптивное меню +4 Выполнено\n
-при нажатии на крестик, или на область вне меню, адаптивное меню плавно скрывается, уезжая за экран +2 Выполнено\n
-ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям при нажатии,\n
- а само адаптивное меню при этом плавно скрывается +2 Выполнено\n
-размеры открытого бургер-меню соответствуют макету, так же открытое бургер-меню 
-проверяется на PixelPerfect +2 Выполнено\n
-ИТОГО 50 БАЛЛОВ
-`);
+function toRight() {
+  offset += 450;
+  slider.style.left = -offset + "px";
+  btnLeft.disabled = false;
+  btnRight.disabled = false;
+  if (offset === 0) {
+    btnLeft.disabled = true;
+  }
+  if (offset === 1800) {
+    btnRight.disabled = true;
+  }
+}
+
+function toLeft() {
+  offset -= 450;
+  slider.style.left = -offset + "px";
+  btnLeft.disabled = false;
+  btnRight.disabled = false;
+  if (offset === 0) {
+    btnLeft.disabled = true;
+  }
+  if (offset === 1800) {
+    btnRight.disabled = true;
+  }
+}
+
+btnRight.addEventListener("click", toRight);
+btnLeft.addEventListener("click", toLeft);
